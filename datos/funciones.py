f858dfa71563c49a5299ddde44c8f1e4a4d2986c
Jaresma <loowcrik@gmail.com>
2019-12-28 00:23:51 -0700

    lkdejfs

---
+++ datos/funciones.py
@@ -0,0 +1,12 @@
+""" la identacion es muy importante """
+def mensaje():
+    print("estamos aprendiendo python")
+def sumasr(num1,num2):
+    print(num1+num2)
+def sumacr(num1,num2):
+    resultado=num1+num2
+    return(resultado)
+
+mensaje()
+sumasr(3,3)
+print(sumacr(4,5))
---
+++ datos/listas.py
@@ -0,0 +1,17 @@
+""" una lista podemos guardar varios espacios en la memoria
+    (son muy parecidos a arrays o vectores)
+    nos permiten guardar cualquier tipo de valores
+    se pueden expandir dinamicamente
+
+    sintaxys
+    nombreLista=[elem1,elem2,elem3]
+    indice(posicion del elemento dentro de la lista)
+
+"""
+
+ListPersons=['maria','pepe','jair','jacobo']
+"""  imprime toda la lista """
+print(ListPersons[:]) 
+""" por posicion(cuenta desde 0) """
+print(ListPersons[2]) 
+
