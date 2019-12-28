89323b40246dba6412df2222e44248a6676409fc
Jaresma <loowcrik@gmail.com>
2019-12-27 23:56:44 -0700

    inivio

---
+++ datos/datos.py
@@ -0,0 +1,18 @@
+print(5+6) 
+print(5%3)
+print(5**3)
+print(9//2)
+
+numero="""esto
+est
+stg
+etg"""
+print(numero)
+type(numero)
+
+numero1=5
+numero2=7
+if numero1>numero2:
+    print("el numero 1 es mayor")
+else:
+    print("el numero 2 es mayor")
